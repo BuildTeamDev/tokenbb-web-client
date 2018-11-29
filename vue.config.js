9ceac19155e7f682166dac739e555457ef3280a9
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-11-29 10:59:46 +0700

    reduce css bundle size

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const { readFileSync } = require( 'fs' );
+const { join } = require( 'path' );
 const webpack = require( 'webpack' );
 const DynamicCdnWebpackPlugin = require( 'dynamic-cdn-webpack-plugin' );
+const PurgecssPlugin = require( 'purgecss-webpack-plugin' );
+const { sync } = require( 'glob-all' );
+
+const maxChunkSize = 290000; // because vue2-editor is about this size and can't be tree-shaken
 
 module.exports = {
   css: {
@@ -10,8 +15,10 @@ module.exports = {
       },
     },
   },
+
   configureWebpack: {
     performance: {
+      maxAssetSize: maxChunkSize,
       maxEntrypointSize: 3000000, // TODO: need to hunt down the bloated package in the future 🔫
     },
     optimization: {
@@ -19,11 +26,18 @@ module.exports = {
       splitChunks: {
         chunks: 'all',
         minSize: 200000,
-        maxSize: 290000,
+        maxSize: maxChunkSize,
       },
     },
     plugins: [
       new webpack.IgnorePlugin( /^\.\/locale$/, /moment$/ ),
+      new PurgecssPlugin( {
+        paths: sync( [
+          join( __dirname, './public/index.html' ),
+          join( __dirname, './src/**/*.vue' ),
+          join( __dirname, './src/**/*.js' ),
+        ] ),
+      } ),
       new DynamicCdnWebpackPlugin( {
         exclude: [ 'buefy' ],
         resolver: require( './scripts/resolveCDN' ),
