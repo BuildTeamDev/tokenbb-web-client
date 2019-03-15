f85213425e3c694630fb8c0645ee82795c8f4706
Cryptoctopus <renaud@buildteam.io>
2019-03-15 06:26:59 -0400

    Create Categories.vue

---
+++ src/views/Categories.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+    <b-table
+      :data="categoryList"
+    >
+      <template slot-scope="props">
+        <b-collapse
+          class="card"
+          aria-id="contentIdForA11y3"
+        >
+          <div
+            slot="trigger"
+            class="card-header"
+            role="button"
+            aria-controls="contentIdForA11y3"
+          >
+            <h1 class="card-header-title">
+              {{ props.row.title }}
+            </h1>
+            <a class="card-header-icon">
+              <b-icon
+                :icon="props.open ? 'menu-down' : 'menu-down'"
+              />
+            </a>
+          </div>
+          <div class="card-content">
+            <div class="columns">
+              <div class="column cat-description">
+                <span class="is-small">{{ props.row.description }}</span>
+              </div>
+            </div>
+          </div>
+        </b-collapse>
+
+        <!--
+        <b-table-column label="Delete" centered>
+
+          <a class="button is-small"
+            :class="{ 'is-loading': fetching }"
+            @click="remove(props.row.id)">
+            <b-icon
+              icon="close-circle"
+              size="is-small">
+            </b-icon>
+          </a>
+        </b-table-column>
+        -->
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+import Table from 'buefy/src/components/table/Table';
+import Icon from 'buefy/src/components/icon/Icon';
+import Collapse from 'buefy/src/components/collapse/Collapse';
+
+export default {
+  name: 'Categories',
+  components: {
+    BTable: Table,
+    BIcon: Icon,
+    BCollapse: Collapse,
+  },
+  data() {
+    return {
+      name: '',
+      title: '',
+      description: '',
+    };
+  },
+  computed: {
+    ...mapState( 'categories', [
+      'categoryList',
+      'fetching',
+    ] ),
+  },
+};
+</script>
